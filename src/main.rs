5a14a408bee89854e273d03698251bce66ad519b
Hashim <13991048+Prim69@users.noreply.github.com>
2022-03-20 18:31:38 -0400

    make devterm add itself to the windows terminal profile list

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod commands;
 use dev_term_io::CommandIo;
 use std::env::{current_dir};
-use std::io::{stdout, stdin, Write};
+use std::fs;
+use std::io::{stdout, stdin, Write, Result};
 use colored::Colorize;
 use std::process::Command;
 use regex::Regex;
+use serde_json::{json, Value};
 
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-const ASCII: &'static str =             r#"           
+const ASCII: &str =             r#"
                                                  ,----,                                  
                                                ,/   .`|                                  
     ,---,                                    ,`   .'  :                           ____   
@@ -29,12 +31,17 @@ const ASCII: &'static str =             r#"
             "#;
 
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
     println!("{}", "         / \\---------------------------,
          \\_,|                          |
             |    Welcome to Dev Term   |
             |  ,-------------------------
             \\_/________________________/ ".green());
+    if cfg!(windows) {
+        if let Err(e) = windows_terminal_profile() {
+            println!("Error setting terminal profile: {}", e);
+        }
+    }
     let regex = Regex::new(r#"(?m)("[^"]+"|[^\s"]+)"#).unwrap();
     loop {
         let wd = current_dir()?;
@@ -64,22 +71,42 @@ fn main() -> std::io::Result<()> {
 
 fn exec_os_command(regex: &regex::Regex, input: String) {
     let mut args = regex.find_iter(input.trim());
-    match args.next() {
-        //check error type as to determine whether we should closest_match
-        Some(name) => {
-            let mut win_cmd = Command::new(name.as_str());
-            let mut vec = vec![];
-            for arg in args {
-                vec.push(arg.as_str())
+    if let Some(name) = args.next() {
+        let mut win_cmd = Command::new(name.as_str());
+        let mut vec = vec![];
+        for arg in args {
+            vec.push(arg.as_str())
+        }
+        match win_cmd.args(vec).status() {
+            Ok(_) => (),
+            Err(e) => {
+                println!("An error occurred while running that command!");
+                println!("Error: {}", e.to_string())
             }
-            match win_cmd.args(vec).status() {
-                Ok(_) => (),
-                Err(e) => {
-                    println!("An error occurred while running that command!");
-                    println!("Error: {}", e.to_string())
-                }
-            }
-        },
-        None => (),
+        }
     }
+}
+
+fn windows_terminal_profile() -> Result<()>{
+    let path = dirs::data_local_dir().expect("no data dir").join("Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json");
+
+    let data = fs::read_to_string(path.clone())?;
+    let mut json: Value = serde_json::from_str(&*data)?;
+    let mut d: Vec<Value> = json["profiles"]["list"].as_array().unwrap().to_vec();
+
+    for i in d.clone() {
+        if i["guid"] == "{577a3f6f-49a4-47c7-bbe5-d698aab06e0e}" {
+            return Ok(())
+        }
+    }
+    d.push(json!({
+        "guid": "{577a3f6f-49a4-47c7-bbe5-d698aab06e0e}",
+        "name": "DevTerm",
+        "commandline": "dev_term.exe",
+        "hidden": false,
+    }));
+
+    json["profiles"]["list"] = Value::Array(d);
+    fs::write(path, serde_json::to_string_pretty(&json).unwrap())?;
+    Ok(())
 }
